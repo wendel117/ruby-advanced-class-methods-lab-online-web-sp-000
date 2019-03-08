0f4833a5d19dfe6c1a30f530501947f7bcddae43
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-08 14:01:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,6 +41,15 @@ end
     self.all.sort_by {|song| song.name}
   end
 
+def self.new_from_filename
+  song_array = filename.split(" - ")
+      song_array[1] = song_array[1].chomp(".mp3")
+      song = self.new
+      song.name = song_array[1]
+      song.artist_name = song_array[0]
+      song
+end
+
   def self.all
     @@all
   end
