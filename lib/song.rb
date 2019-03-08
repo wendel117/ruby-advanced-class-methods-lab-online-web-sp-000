8c5c42d3ef2b6e9a099e759bdb5aa26d9d444d83
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-08 14:03:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,11 +43,11 @@ end
 
 def self.new_from_filename
   song_array = filename.split(" - ")
-      song_array[1] = song_array[1].chomp(".mp3")
-      song = self.new
-      song.name = song_array[1]
-      song.artist_name = song_array[0]
-      song
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
 end
 
   def self.all
