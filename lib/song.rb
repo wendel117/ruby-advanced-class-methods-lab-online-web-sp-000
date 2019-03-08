8e6d7315ac47e291462876b89012f8e412f07c39
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-08 13:39:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,22 +6,26 @@ def save
   self.class.all << self
 end
 
-  def self.create
+  def self.create #working
      song = self.new
      self.all << song
      song
    end
 
-   def self.new_by_name(name) #class constructor
+   def self.new_by_name(name) #class constructor working
      song = self.new
      song.name = name
      song
    end
 
-  def self.create_by_name(name)
-    song = self.create
-    song.name = name
-    song
+  def self.create_by_name(name) #working
+    song = self.create #allows for a class instance to be created
+    song.name = name #assigns the reader/writer 
+    song #returns the new song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
   end
 
   def self.all
